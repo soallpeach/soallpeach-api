dea9a7915533dd63e977b69b486c71b529a9824b
soroosh <soroosh.sarabadani@gmail.com>
2020-04-02 23:10:27 +0200

    score fields

--- scores/models.py
+++ scores/models.py
@@ -4,4 +4,7 @@ from django_mysql.models import JSONField, Model
 
 class Score(Model):
     nickname = models.CharField(max_length=200)
+    challenge_name = models.CharField(max_length=100)
+    created = models.DateTimeField(auto_now_add=True)
+    run_id = models.IntegerField()
     result = JSONField()
