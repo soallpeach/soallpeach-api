f54bf6c36bc543fe429118a9bf41986ea3a3d972
soroosh <soroosh.sarabadani@gmail.com>
2020-04-05 00:36:25 +0200

    add state to the model

--- scores/models.py
+++ scores/models.py
@@ -8,3 +8,4 @@ class Score(Model):
     created = models.DateTimeField(auto_now_add=True)
     run_id = models.IntegerField()
     result = JSONField()
+    state = models.CharField(max_length=30, choices=[('PASSED', 'PASSED'), ('FAILED', 'FAILED')])
