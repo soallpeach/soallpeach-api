20ff4f7559ed660ea5317e2d8ad6db10af6e4470
soroosh <soroosh.sarabadani@gmail.com>
2020-04-13 14:08:51 +0200

    Round model

--- scores/models.py
+++ scores/models.py
@@ -2,10 +2,16 @@ from django.db import models
 from django_mysql.models import JSONField, Model
 
 
-class Score(Model):
-    nickname = models.CharField(max_length=200)
+class Round(Model):
     challenge_name = models.CharField(max_length=100)
     created = models.DateTimeField(auto_now_add=True)
-    run_id = models.IntegerField()
+    state = models.CharField(max_length=30, choices=[('STARTED', 'STARTED'), ('FINISHED', 'FINISHED')],
+                             default='FINISHED')
+
+
+class Score(Model):
+    nickname = models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
     result = JSONField()
     state = models.CharField(max_length=30, choices=[('PASSED', 'PASSED'), ('FAILED', 'FAILED')])
+    round = models.ForeignKey(to=Round, on_delete=models.CASCADE, default=None, null=True)
