3c8bc69c8b9bad3e63e5d3c0a74202be80cbe803
soroosh <soroosh.sarabadani@gmail.com>
2020-04-13 15:10:21 +0200

    Round views

--- scores/views.py
+++ scores/views.py
@@ -2,18 +2,41 @@ from django.shortcuts import render
 from django.views import View
 from django_mysql.models.functions import JSONExtract
 from rest_framework import status
+from rest_framework.generics import GenericAPIView
+from rest_framework.mixins import UpdateModelMixin
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from scores.models import Score
-from scores.serializers import ScoreSerializer
+from scores.models import Score, Round
+from scores.serializers import ScoreSerializer, RoundSerializer
 from scores.util import convert_ms_to_minutes
 import time
 
 
+class RoundView(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def post(self, request, challenge_name):
+        serializer = RoundSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class RoundDetailView(GenericAPIView, UpdateModelMixin):
+    permission_classes = (IsAuthenticated,)
+    queryset = Round.objects.all()
+    serializer_class = RoundSerializer
+    lookup_field = 'id'
+
+    def patch(self, request, *args, **kwargs):
+        return self.partial_update(request, *args, **kwargs)
+
+
 class ScoreView(APIView):
-    permission_classes = (IsAuthenticated,)  # <-- And here
+    permission_classes = (IsAuthenticated,)
 
     def get(self, request):
         return Response(ScoreSerializer(instance=Score.objects.all(), many=True).data)
