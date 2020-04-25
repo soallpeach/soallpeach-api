6743fbde19bcc9cac1b996f8afa5e627e6beb197
soroosh <soroosh.sarabadani@gmail.com>
2020-04-25 23:20:40 +0200

    new way process the scores

---
+++ scores/score_processor.py
@@ -0,0 +1,43 @@
+import json
+import time
+
+from django_mysql.models.functions import JSONExtract
+
+from scores.models import Score, Round
+from scores.util import convert_ms_to_minutes
+
+
+def process(score: Score) -> Score:
+    if score.round.challenge_name == 'countme':
+        return process_countme(score)
+    return score
+
+
+def process_countme(score: Score) -> Score:
+    metrics_str = score.result.get('metrics', {}).get('stdout', {})
+    metrics_json = json.loads(metrics_str)
+    status_codes = metrics_json.get('status_codes', {})
+    if len(status_codes) != 1:
+        score.state = 'FAILED'
+    else:
+        count_of_200 = status_codes.get('200', None)
+        if not count_of_200:
+            score.state = 'FAILED'
+
+    if score.state != 'FAILED':
+        p95_latency_ns = metrics_json.get('latencies', {}).get('95th', 999999999)
+        score.main_indicator = p95_latency_ns / 1_000_000.0
+    return score
+
+
+def prepare_scores(challenge_name: str):
+    latest_round = Round.objects.filter(challenge_name=challenge_name, state='FINISHED').last()
+    if latest_round:
+        time_passed_from_last_run = convert_ms_to_minutes((time.time() - latest_round.updated.timestamp()) * 1000)
+        latest_scores = Score.objects.filter(round_id=latest_round.id).order_by(
+            JSONExtract('result', '$.run_result.duration').asc(nulls_last=True)
+        ).all()
+    else:
+        time_passed_from_last_run = 0
+        latest_scores = []
+    return latest_scores, time_passed_from_last_run
